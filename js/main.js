b068a89c89e91b61f016f842e7df36249c60e51c
Dan Shea <shea.dan@gmail.com>
2016-03-19 16:08:09 -0700

    Update
    Minor update, changed minor elements for acquire form and mocked up
    quick data analysis

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@
  * Main AngularJS Web Application
  */
 var app = angular.module('ecmWebApp', [
-  'ngRoute'
+  'ngRoute','chart.js'
 ]);
 
 /**
@@ -58,7 +58,36 @@ app.controller('TabController', function(){
   this.isSet = function(tabName){
     return this.tab === tabName;
   };
+  });
 
 
+app.controller("LineCtrl", function ($scope) {
+
+    $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+    $scope.series = ['Series A', 'Series B'];
+    $scope.data = [
+      [65, 59, 80, 81, 56, 55, 40],
+      [28, 48, 40, 19, 86, 27, 90]
+    ];
+    $scope.onClick = function (points, evt) {
+      console.log(points, evt);
+    };
+  });
+
+app.controller("FileScanner", function ($scope) {
+
+  $scope.files = [
+    {'name':'file_1', 'date':'03-Feb-2016'},
+    {'name':'file_2', 'date':'04-Feb-2016'},
+    {'name':'file_3', 'date':'07-Feb-2016'},
+    {'name':'file_4', 'date':'10-Mar-2016'}
+    ];
+
+  $scope.selected = null;
+
+  $scope.setSelected = function($index) {
+    $scope.selected = $index;
+    console.log($scope.selected);
+};
 
 });
